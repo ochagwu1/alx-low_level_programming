02e12318d937e433edf6d7ced7771b63838ff7f0
ochagwu1 <ochagwu1@gmail.com>
2022-10-17 16:07:42 -0700

    i think something is wrong with the checker

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -3,8 +3,6 @@
 /**
  * main - print the size of various types
  *
- * Description: prints the size of various types on the computer it is compiled and run on
- *
  * Return: Allways 0 (Success)
  */
 
